7ada04b897760db7bd6975c0d93ee2d89cfb9852
changyun <qkrcksrbs8@naver.com>
2021-12-25 16:22:37 +0900

    Aop로 공통 로그 구현

---
+++ src/main/java/com/example/configclient/aop/AopConfig.java
@@ -0,0 +1,59 @@
+package com.example.configclient.aop;
+
+import com.example.configclient.util.LogUtil;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Aspect
+@Component
+
+public class AopConfig {
+
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+    String sessGuid = "E6F9B4F2A2465CE76D991BF5807AD9E0";
+
+    @Around("execution(* com.example.configclient..controller.*.*(..)) || execution(* com.example.configclient..service.*.*(..))")
+    public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
+        String type = joinPoint.getSignature().toShortString();
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        request.getParameterMap();
+        logger.info("SESS_GUID = {}, ===================START===================", sessGuid);
+        logger.info("SESS_GUID = {}, @Around : {} ", sessGuid, type);
+        logger.info("SESS_GUID = {}, @Around : {}, param : {} ", sessGuid, type, LogUtil.mapToStr(request.getParameterMap()));
+        return joinPoint.proceed();
+    }
+
+    @Before("execution(* com.example.configclient..controller.*.*(..)) || execution(* com.example.configclient..service.*.*(..))")
+    public void before(JoinPoint joinPoint) {
+        String type = joinPoint.getSignature().toShortString();
+        Object[] args = joinPoint.getArgs();
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        if (args.length == 0 || args[0] instanceof HttpServletRequest){
+            logger.info("SESS_GUID = {}, @Before : {}, param : {}", sessGuid, type, LogUtil.mapToStr(request.getParameterMap()));
+        }else{
+            logger.info("SESS_GUID = {}, @Before : {}, param : {}", sessGuid, type, LogUtil.setParamParse(args[0].toString(), "="));
+        }
+    }
+
+    @AfterReturning(pointcut = "execution(* com.example.configclient..controller.*.*(..)) || execution(* com.example.configclient..service.*.*(..))", returning="retValue")
+    public void after(JoinPoint joinPoint, Object retValue) {
+        String type = joinPoint.getSignature().toShortString();
+        logger.info("SESS_GUID = {}, @After : {}, result : {}", sessGuid, type, retValue);
+        logger.info("SESS_GUID = {}, ===================E N D===================", sessGuid);
+    }
+
+    @AfterThrowing(pointcut = "execution(* com.example.configclient..controller.*.*(..)) || execution(* com.example.configclient..service.*.*(..))", throwing = "ex")
+    public void afterThrowingAnException(JoinPoint joinPoint, Exception ex) {
+        logger.info("SESS_GUID = {}, ===================E N D===================", sessGuid);
+    }
+
+
+}
